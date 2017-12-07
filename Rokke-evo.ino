7504d31c1b9f08b8895057d8776c7b1f499c0242
Frank Veenstra <frve>
2017-12-07 11:54:14 +0100

    Optimized the program to work with 6 servo motors. Serial communication program is not included but will be added in the next version

--- Rokke-evo.ino
+++ Rokke-evo.ino
@@ -2,22 +2,23 @@
 
 int amountServos = 6;
 Servo servos[6]; // six servos in array
-int curPos[6] = {90,90,90}; // stores servo positions
+int curPos[6] = {90, 90, 90}; // stores servo positions
 
 int geneLength = 10; // needs to be an even number
 int gene[10];
 int servoCenter = 90;
 int maxServoRange = 30; // only move maxServoRange from center
-int stepSize = 1;
+int stepSize = 10;
 
 #define PI2 6.283185 // 2*PI saves calculation later
 
 int delayTime = 10; //Delay between each sample when playing back movement (for Tower Pro SG90: At 4.8V, the speed of the servo is 0.12 sec/60° = 2 ms/deg.)
 int P = 400; //Maximum period of the periodic movement function
 int playBackTime = 2 * P; //The movement pattern will be played back two times when it is being evalutated
-#define LENGTH 200 // Length of the wave lookup table - needs to be equal to P/delayTime
-byte wave[LENGTH]; // Storage for waveform to play back from
+#define LENGTH 500 // Length of the wave lookup table - needs to be equal to P/delayTime
+byte wave[6][LENGTH]; // Storage for waveform to play back from
 float f; //Temporary storage for calculating function values
+int offsets[6];
 
 int sensorPin = A0; // select the input pin for LDR (or strain sensor)
 int sensorValue = 0; // variable to store the value coming from the sensor
@@ -33,23 +34,34 @@ void setup() {
 }
 
 void loop() {
+  int n = 0;
+  bool eval = false; 
   while (Serial.available() > 0) {
-    delay(500); // ensure entire serial signal is received, not half a signal
-    for (int n = 0; n < geneLength; n++) {
+    delay(20); // ensure entire serial signal is received, not half a signal
+    if (n < geneLength) {
       gene[n] = Serial.read();
       Serial.print(gene[n]);
       Serial.print(",");
+      eval = true; 
     }
+    else {
+      Serial.read();
+    }
+    n++;
+  }
+  if (eval == true) {
     delay(100);
     float fitness = evaluateIndividual(gene);
     Serial.println(fitness);
     delay(100);
     setServosToPosition(servoCenter);
     delay(100);
+    eval = false; 
   }
 }
 
 void setServosToPosition(int pos) {
+  // smoothly set servo to desired position
   for (int n = 0; n < 10; n++) {
     for (int i = 0; i < amountServos; i++) {
       int dpos = pos - curPos[i];
@@ -69,27 +81,49 @@ float evaluateIndividual(int gene[]) {
   //  [copy code here from Franks program to receive values]
   // *2* Calculate array with movement data based on output from NN
   //     (done by a superposition of ten sine waves, i.e. the first ten terms of the Fourier series)
-
-  for (int i = 0; i < LENGTH; i++) { // Step across wave table
-    float sum = 0; //Temporary storage for calculating sum of function values
-    for (int n = 0; n < geneLength; n += 2) {
-      //Standard form of the Fourier series is: sN(x)= A0/2 +Sum(n=1toN)( An*sin(2*pi*n*x/P+phi[n]) );
-      //   [N is fixed to 10]
-      //   [phi(n) is confined to - [0;2*pi]]
-      //   [x is equal to i]
-      //   [A0 is set to 90. (Should it be able to vary? Now the fin movements always oscillate around 90 degrees)]
-      //   [An is set to vary in the interval between 0 and maxAngleChange]
-      f = gene[n] * sin(((PI2 * (n + 1) * (i + 1)) / P) + gene[n + 1]); //+1 added to n and i to avoid zero for first value
-      sum = sum + f;
+  for (int j = 0; j < amountServos; j++) {
+    for (int i = 0; i < LENGTH; i++) { // Step across wave table
+      float sum = 0; //Temporary storage for calculating sum of function values
+      for (int n = 0; n < geneLength; n += 2) {
+        //Standard form of the Fourier series is: sN(x)= A0/2 +Sum(n=1toN)( An*sin(2*pi*n*x/P+phi[n]) );
+        //   [N is fixed to 10]
+        //   [phi(n) is confined to - [0;2*pi]]
+        //   [x is equal to i]
+        //   [A0 is set to 90. (Should it be able to vary? Now the fin movements always oscillate around 90 degrees)]
+        //   [An is set to vary in the interval between 0 and maxAngleChange]
+        // f = gene[n] * sin(((PI2 * (n + 1) * (i + 1)) / P) + gene[n + 1]); //+1 added to n and i to avoid zero for first value
+        float AMP = ((float)gene[n] / 256.0) * 30.0;
+        float PHASE = 1;//gene[n + 1];
+        float FREQ = 1.0/(float)gene[n+1];//gene[n + 2];
+        //if (j >=3) {
+        f = AMP * sin((PI2 / 2) * ((float) (-i * FREQ)) + PHASE + j); //+1 added to n and i to avoid zero for first value
+        //}
+        //else {
+        //  f = AMP * sin((PI2 / 2) * ((float) (-i * FREQ)));// + PHASE -j); //+1 added to n and i to avoid zero for first value          
+        //}
+        //Serial.print("AMP:");
+        //Serial.print(AMP);
+        //Serial.print(",PHASE:");
+        //Serial.print(PHASE);
+        //Serial.print(",FREQ:");
+        //Serial.print(FREQ);
+        //Serial.print(",f:");
+        //Serial.print(f);
+        sum = sum + f;
+      }
+      sum = 90.0 + (sum / (geneLength / 2.0)) + 0.5; //0.5 added to round off correctly when converting to byte below
+      wave[j][i] = (byte)sum;
+      //Serial.print("wave ");
+      //Serial.print(j);
+      //Serial.print(":");
+      //Serial.print(wave[j][i]);
     }
-    sum = 90 + (sum / (geneLength / 2)) + 0.5; //0.5 added to round off correctly when converting to byte below
-    wave[i] = (byte)sum;
   }
 
   //*3* Play back movement on robot from wavetable and sum up the sensor readings
-  for (int i = 0; i < LENGTH - (amountServos * stepSize); i += stepSize) { // Step across wave table
+  for (int i = 0; i < LENGTH; i++) { // Step across wave table
     for (int n = 0; n < amountServos; n++) {
-      int servoPos = wave[i + (stepSize * amountServos)];
+      int servoPos = wave[n][i];
       Serial.print(servoPos);
       Serial.print(",");
       if (servoPos < servoCenter - maxServoRange) {
